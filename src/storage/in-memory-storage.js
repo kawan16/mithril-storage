5a2a9e8f069f2f5d6ec8ed0aea9bb03dec4166c7
devooght.k <devooght.k@sfeir.lu>
2015-05-08 17:11:38 +0200

    Initial commit

---
+++ src/storage/in-memory-storage.js
@@ -0,0 +1,36 @@
+
+
+    /**
+     * Default constructor of in-memory Storage
+     */
+    function InMemoryStorage( ) {
+        this.$storage = {};
+    }
+
+   /**
+    * Stores the value for a given key
+    * @param {string} key   a key
+    * @param {object} value a value
+    */
+    InMemoryStorage.prototype.set = function ( key , value ) {
+        this.$storage[ key ] = JSON.stringify( value );
+    };
+
+
+   /**
+    * Returns the value given a key
+    * @param {string} key a key
+    */
+    InMemoryStorage.prototype.get = function ( key ) {
+        return  JSON.parse( this.$storage[ key ] );
+    };
+
+   /**
+    * Removes the key from the store
+    * @param {string} key a key
+    */
+    InMemoryStorage.prototype.remove = function ( key ) {
+        delete this.$storage[ key ];
+    };
+
+
